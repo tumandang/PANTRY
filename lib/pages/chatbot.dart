884ec91c50b78c51faa91bd00bd362365c22db04
Danish <danishdanial0991@gmail.com>
2025-10-08 12:54:43 +0800

    Chatbot page

--- lib/pages/chatbot.dart
+++ lib/pages/chatbot.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
+class ChatbotPage extends StatelessWidget {
+  const ChatbotPage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Borrow',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'CalSans'
+          ),
+        ),
+      ),
+    );
+  }
+}
 
